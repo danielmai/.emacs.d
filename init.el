90a76ef4b626e9ab94d8cd5cec5fec68244b9a21
Daniel Mai <daniel@udacity.com>
2015-01-23 09:20:20 -0800

    Edit initial window-system configuration file.
    It looks cleaner and more explicit when these
    methods will be bound.

--- init.el
+++ init.el
@@ -1,7 +1,10 @@
 ;; Turn off mouse interface early in startup to avoid momentary display
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(when window-system
+  (menu-bar-mode -1)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
+  (tooltip-mode -1))
+
 
 (setq inhibit-startup-message t)
 (setq initial-scratch-message "")
