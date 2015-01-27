5db71c92c626404b62bbe2237b52bd6c996dbe6c
Daniel Mai <daniel@udacity.com>
2015-01-26 23:08:03 -0800

    Add a whole ton of configurations.
    Yay for making an acceptable configuration set up.
    
    Now I need to refine it and add the other necessary auxillary files.

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+;;; Begin initialization
 ;; Turn off mouse interface early in startup to avoid momentary display
 (when window-system
   (menu-bar-mode -1)
@@ -5,20 +6,22 @@
   (scroll-bar-mode -1)
   (tooltip-mode -1))
 
-
 (setq inhibit-startup-message t)
 (setq initial-scratch-message "")
 
+;;; Set up package
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") )t
 (package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
 
+;;; Bootstrap use-package
+;; Install use-package if it's not already installed.
+;; use-package is used to configure the rest of the packages.
 (unless (package-installed-p 'use-package)
+  (package-refresh-contents)
   (package-install 'use-package))
-
 (require 'use-package)
 
+;;; Load the config
 (org-babel-load-file (concat user-emacs-directory "config.org"))
