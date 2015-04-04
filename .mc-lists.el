07d0775e9e6d0ffbacddbdd5c1bfb5655509c333
Daniel Mai <daniel@danielmai.net>
2015-04-04 11:06:40 -0700

    Add more to .mc-lists.el.

--- .mc-lists.el
+++ .mc-lists.el
@@ -4,26 +4,33 @@
 (setq mc/cmds-to-run-for-all
       '(
         beginning-of-visual-line
+        c-electric-colon
         c-electric-delete-forward
         c-electric-paren
         c-electric-semi&comma
         c-electric-slash
-        delete-horizontal-space
         dabbrev-expand
+        delete-horizontal-space
         electric-newline-and-maybe-indent
         end-of-visual-line
         helm-confirm-and-exit-minibuffer
+        indent-for-tab-command
         kill-visual-line
         mark-whole-buffer
+        markdown-insert-bold
         mc/mark-next-line-like-this
         org-beginning-of-line
+        org-ctrl-c-ctrl-c
         org-delete-char
         org-end-of-line
         org-self-insert-command
         sgml-close-tag
+        sgml-slash
+        transpose-words
         yas-expand
         ))
 
 (setq mc/cmds-to-run-once
       '(
+        markdown-exdent-or-delete
         ))
