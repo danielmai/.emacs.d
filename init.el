57dc81397c4fce24981a94d51864022f3d155825
Daniel Mai <daniel@danielmai.net>
2017-06-24 17:12:56 -0700

    Add 'org' package archive.
    Pin org-plus-contrib so that that package is only installed from the
    org archive.

--- init.el
+++ init.el
@@ -16,6 +16,10 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(when (boundp 'package-pinned-packages)
+  (setq package-pinned-packages
+        '((org-plus-contrib . "org"))))
 (package-initialize)
 
 ;;; Bootstrap use-package
